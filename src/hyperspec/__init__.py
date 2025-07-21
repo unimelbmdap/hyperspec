9a932c2ca770a0573770467fa6ada5fa01110c5f
Simon Mutch <smutch@unimelb.edu.au>
2025-07-21 10:32:35 +1000

    Expose io and stats modules in public API
    Add direct imports for io and stats modules and include them in __all__
    to make them available at package level.
    
    closes #1

--- src/hyperspec/__init__.py
+++ src/hyperspec/__init__.py
@@ -1,3 +1,4 @@
+from . import io, stats
 from .io import read_specim, read_specim_manifest
 
-__all__ = ["read_specim", "read_specim_manifest"]
+__all__ = ["read_specim", "read_specim_manifest", "io", "stats"]
